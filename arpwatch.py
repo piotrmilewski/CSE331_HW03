455efa8cb2b941dc264f44252abf5a0dea82d37e
Piotr Milewski <piotr250tiger@gmail.com>
2020-10-31 18:28:30 -0400

    finished arpwatch

--- arpwatch.py
+++ arpwatch.py
@@ -1,6 +1,5 @@
 from python_arptable import get_arp_table
 from scapy.all import *
-from scapy.layers.inet import *
 from scapy.layers.l2 import *
 
 orig_arp_table = []
@@ -20,13 +19,30 @@ def checkPacket(pkt):
         if orig_mac is None:
             return
         curr_mac = pkt[ARP].hwsrc
-        print(curr_mac)
         if orig_mac != curr_mac:
             print(srcIP + " changed from " + orig_mac + " to " + curr_mac)
 
 
 def main(argv):
-    sniff(prn=checkPacket, filter="arp", store=0)
+    pOpt = "enp0s8"
+
+    # get input from command line
+    try:
+        opts, args = getopt.getopt(argv, "i:")
+    except getopt.GetoptError:
+        print('USAGE: arpwatch.py [-i interface]')
+        sys.exit(1)
+
+    for opt, arg in opts:
+        if opt == '-i':
+            pOpt = arg
+
+    # sniff for ARP packets
+    try:
+        sniff(prn=checkPacket, iface=pOpt, filter="arp", store=0)
+    except OSError:
+        print("Invalid interface, please use 'arp' to determine the correct interface to use")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
